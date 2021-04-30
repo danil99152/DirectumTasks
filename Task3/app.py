96ac7cdc33abd261716e60b9cceb1a1a1ac73efa
Danil Komyshev <40982684+danil99152@users.noreply.github.com>
2021-05-01 01:53:39 +0400

    Исправил ошибку чтения ссылок

--- Task3/app.py
+++ Task3/app.py
@@ -11,11 +11,10 @@ model = keras.models.load_model('C:/Users/danil99152/Downloads/model2.h5')
 
 @app.route('/<path:file>')
 def upload_page(file):
+    file = file.replace("/", "\\")
     doc = cv2.imread(file)
-    print(type(doc))
     height, width, channels = doc.shape
     doc = cv2.resize(doc, (256, 256)) / 255.
-    print(type(doc))
     pred = model.predict(np.expand_dims(doc, axis=0))
     doc_type = "main"
     if pred >= 0.5:
