952f9f36ec909a4f465c6a273b6c665846085107
Danil Komyshev <40982684+danil99152@users.noreply.github.com>
2021-05-01 00:09:13 +0400

    Мелкие правки ошибок, сделанных впопыхах

--- Task3/app.py
+++ Task3/app.py
@@ -1,12 +1,13 @@
 import cv2
 from tensorflow import keras
-
+import numpy as np
 from flask import Flask
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 model = keras.models.load_model('C:/Users/danil99152/Downloads/model2.h5')
+
 
 @app.route('/<path:file>')
 def upload_page(file):
@@ -15,9 +16,9 @@ def upload_page(file):
     height, width, channels = doc.shape
     doc = cv2.resize(doc, (256, 256)) / 255.
     print(type(doc))
-    pred = model.predict(doc)
+    pred = model.predict(np.expand_dims(doc, axis=0))
     doc_type = "main"
-    if pred == 1:
+    if pred >= 0.5:
         doc_type = "other"
     return {
         "source": {
