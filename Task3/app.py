fab3a3cca955882e9c23226c7c5e501d46cdcb27
Danil Komyshev <40982684+danil99152@users.noreply.github.com>
2021-04-30 23:34:20 +0400

    Добавил веб-сервер в задачу 3

---
+++ Task3/app.py
@@ -0,0 +1,32 @@
+import cv2
+from tensorflow import keras
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+model = keras.models.load_model('C:/Users/danil99152/Downloads/model2.h5')
+
+@app.route('/<path:file>')
+def upload_page(file):
+    doc = cv2.imread(file)
+    print(type(doc))
+    height, width, channels = doc.shape
+    doc = cv2.resize(doc, (256, 256)) / 255.
+    print(type(doc))
+    pred = model.predict(doc)
+    doc_type = "main"
+    if pred == 1:
+        doc_type = "other"
+    return {
+        "source": {
+            "width": width,
+            "height": height,
+            "type": doc_type
+        }
+    }
+
+
+if __name__ == '__main__':
+    app.run()
