3b2aeeb729ec8596d83a3f44aba6be1f3157e156
Danil Komyshev <40982684+danil99152@users.noreply.github.com>
2021-05-03 23:59:36 +0400

    Сделал, но без OCR

--- Task4/app.py
+++ Task4/app.py
@@ -1,19 +1,7 @@
 import spacy as spacy
-from pytesseract import Output
-
-try:
-    from PIL import Image
-except ImportError:
-    import Image
-
-import pytesseract
 from flask import Flask
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\danil99152\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-
 nlp = spacy.load("en_core_web_sm")
-
-UPLOAD_FOLDER = 'images/'
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -48,33 +36,11 @@ def get_facts(doc):
     return facts
 
 
-def ocr_core(file):
-    io = Image.open(file)
-    text = pytesseract.image_to_string(io, lang='eng')
-    return text
-
-
 @app.route('/<seq>')
 def upload_page(seq):
-    print(seq)
-    if seq.rsplit('.', 1)[1].lower() == "tif":
-        filename = seq.rsplit('/', 1)[-1].lower()
-        tiffile = UPLOAD_FOLDER + filename.replace(filename.rsplit('.', 1)[1].lower(), 'jpg')
-        try:
-            im = Image.open(seq)
-            im.thumbnail(im.size)
-            im.save(tiffile, "JPEG", quality=100)
-        except Exception as e:
-            print(e)
-        seq = tiffile
-
-        extracted_text = ocr_core(seq)
-        doc = nlp(extracted_text)
-    else:
-        doc = nlp(seq)
-
+    doc = nlp(seq)
     facts = get_facts(doc)
-    #
+
     return {
         "facts": facts
     }
